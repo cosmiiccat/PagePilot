86f1f258bd21557d6f92cc62eacf4be990e00a4a
preetam <preetam6teen@gmail.com>
2024-06-13 17:53:43 +0530

    Updated user models and book models - Preetam Pati

--- masterservice/service/models.py
+++ masterservice/service/models.py
@@ -1,12 +1,52 @@
 from django.db import models
+from typecheckers import Type
+import uuid
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=13, unique=True)
-    availability = models.BooleanField(default=True)
+class Book:
 
-    def __str__(self):
-        return self.title
+    @staticmethod
+    def schema(title, author, isbn, availability):
+    
+        try: 
+            title = Type.CharField(title, f"{'Book'}.{'title'}", max_length=255)
+            author = Type.CharField(author, f"{'Book'}.{'author'}",max_length=255)
+            isbn = Type.CharField(isbn, f"{'Book'}.{'isbn'}",max_length=13)
+            availability = Type.BooleanField(availability, f"{'Book'}.{'availability'}")
+
+            return {
+                '_id': str(uuid.uuid4()),
+                'title': title, 
+                'author': author,
+                'isbn': isbn, 
+                'availability': availability
+            }
+        
+        except Exception as e: 
+            return e
+        
+class User:
+
+    @staticmethod
+    def schema(name, user_id, phone, email):
+    
+        try: 
+            name = Type.CharField(name, f"{'User'}.{'name'}", max_length=255)
+            user_id = Type.CharField(user_id, f"{'User'}.{'user_id'}",max_length=10)
+            phone = Type.CharField(phone, f"{'User'}.{'phone'}",max_length=13)
+            email = Type.CharField(email, f"{'User'}.{'email'}",max_length=25)
+
+            return {
+                '_id': str(uuid.uuid4()),
+                'name': name, 
+                'user_id': user_id,
+                'phone': phone, 
+                'email': email,
+                'past_issue': [],
+                'current_issue': [] 
+            }
+        
+        except Exception as e: 
+            return e
+
 
 
--- masterservice/service/typecheckers.py
+++ masterservice/service/typecheckers.py
@@ -1,10 +1,42 @@
 from utils import custom_exceptions
+# import custom_exceptions
 
-class TypeChecker:
+class Type:
 
-    def __init__():
-        pass
-
+    @staticmethod
     def IntegerField(data, schema_name): 
-        if type(data) != int: 
-            raise custom_exceptions.CustomError(f'Error not int')
+        if not isinstance(data, int):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be an integer, but received {type(data).__name__}.')
+        return data 
+        
+    @staticmethod
+    def CharField(data, schema_name, max_length = 100): 
+        if not isinstance(data, str):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a string, but received {type(data).__name__}.')
+        if len(data) > max_length:
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a {max_length} length string, but received {len(data)} length string.')
+        return data 
+    
+    @staticmethod
+    def BooleanField(data, schema_name):
+        if not isinstance(data, bool):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a boolean, but received {type(data).__name__}.')
+        return data
+
+    @staticmethod
+    def FloatField(data, schema_name):
+        if not isinstance(data, float):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a float, but received {type(data).__name__}.')
+        return data
+
+    @staticmethod
+    def ListField(data, schema_name):
+        if not isinstance(data, list):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a list, but received {type(data).__name__}.')
+        return data
+
+    @staticmethod
+    def DictField(data, schema_name):
+        if not isinstance(data, dict):
+            raise custom_exceptions.CustomError(f'The field "{schema_name}" should be a dictionary, but received {type(data).__name__}.')
+        return data
